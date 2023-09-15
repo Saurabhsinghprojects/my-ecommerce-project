d5cb1695f9c14d500ba85882044b21ca6af2449d
Saurabh_iitism <74338540+Saurabhsinghprojects@users.noreply.github.com>
2023-09-15 17:17:01 +0530

    Update Contact.js

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -5,9 +5,7 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:20je0881@me.iitism.ac.in">
-        <Button>Contact: 20JE0881@me.iitism.ac.in</Button>
-      </a>
+    Contact: 20JE0881@me.iitism.ac.in
     </div>
   );
 };
